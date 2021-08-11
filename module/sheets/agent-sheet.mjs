15c73e0211687c0ba530cbded06c2b06e3fce962
John C Barstow <jcb@jbowtie.geek.nz>
2021-08-11 23:15:41 +1200

    Handle the 0 dice case

--- module/sheets/agent-sheet.mjs
+++ module/sheets/agent-sheet.mjs
@@ -32,16 +32,27 @@ export class AgentSheet extends ActorSheet {
     async _onRoll(event) {
         event.preventDefault();
         
-        let speaker = ChatMessage.getSpeaker();
+        let speaker = ChatMessage.getSpeaker(this.actor);
         const action = $(event.currentTarget).data("action");
         const data = this.actor.data.data;
         let dice_amount = data.actions[action].value;
+        let zeromode = false;
+        if (dice_amount < 0) { dice_amount = 0;}
+        if (dice_amount <= 0) {
+            dice_amount = 2;
+            zeromode = true;
+        }
         let r = new Roll( `${dice_amount}d6`, {} );
-        //r.roll().toMessage({speaker: speaker, flavor: action});
+
         await r.evaluate({async: true});
         let rolls = (r.terms)[0].results;
-        let roll_status = this.getActionRollStatus(rolls, false);
-        let result = await renderTemplate("systems/buried-secrets/templates/secrets-roll.html", {rolls: rolls, roll_status: roll_status, zeromode: false});
+        let roll_status = this.getActionRollStatus(rolls, zeromode);
+        let result = await renderTemplate("systems/buried-secrets/templates/secrets-roll.html", {
+            rolls: rolls, 
+            roll_status: roll_status, 
+            zeromode: zeromode, 
+            action: action});
+
         let messageData = {
             speaker: speaker,
             content: result,
@@ -54,34 +65,34 @@ export class AgentSheet extends ActorSheet {
 
     getActionRollStatus(rolls, zeromode = false) {
 
-  let sorted_rolls = rolls.map(i => i.result).sort();
-  let roll_status;
-  let use_die;
-  let prev_use_die;
+        let sorted_rolls = rolls.map(i => i.result).sort();
+        let roll_status;
+        let use_die;
+        let prev_use_die;
 
-  if (zeromode) {
-    use_die = sorted_rolls[0];
-  } else {
-    use_die = sorted_rolls[sorted_rolls.length - 1];
-    if (sorted_rolls.length - 2 >= 0) {
-      prev_use_die = sorted_rolls[sorted_rolls.length - 2]
-    }
-  }
+        if (zeromode) {
+            use_die = sorted_rolls[0];
+        } else {
+            use_die = sorted_rolls[sorted_rolls.length - 1];
+            if (sorted_rolls.length - 2 >= 0) {
+                prev_use_die = sorted_rolls[sorted_rolls.length - 2]
+            }
+        }
 
-  // 1,2,3 = failure
-  if (use_die <= 3) {
-    roll_status = "failure";
-  } else if (use_die === 6) {
-    // if 6 - check the prev highest one.
-    // 6,6 - critical success
-    if (prev_use_die && prev_use_die === 6) {
-      roll_status = "critical-success";
-    } else {
-    // 6 - success
-      roll_status = "success";
-    }
-  } else {
-    // else (4,5) = partial success
+        // 1,2,3 = failure
+        if (use_die <= 3) {
+            roll_status = "failure";
+        } else if (use_die === 6) {
+            // if 6 - check the prev highest one.
+            // 6,6 = critical success
+            if (prev_use_die && prev_use_die === 6) {
+                roll_status = "critical-success";
+            } else {
+                // 6 = success
+                roll_status = "success";
+            }
+        } else {
+            // else (4,5) = partial success
     roll_status = "partial-success";
   }
 
